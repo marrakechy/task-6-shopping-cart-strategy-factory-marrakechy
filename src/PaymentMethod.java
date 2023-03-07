8e6b72e36f3ac445735405661e0c81cc66fd18e1
marrakechy <belmarrakechy21@coe.edu>
2023-03-07 17:17:56 -0600

    Commit

--- src/PaymentMethod.java
+++ src/PaymentMethod.java
@@ -1,3 +1,19 @@
-public interface PaymentMethod {
-    boolean pay(double amt);
+import hb.PaymentMethod;
+
+public class PayBuddyMethod implements PaymentMethod {
+
+    private String email;
+
+    public PayBuddyMethod(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public boolean pay(double amt) {
+        System.out.println("Confirming payment through PayBuddy");
+        System.out.println("Amount: " + amt);
+        System.out.println("Using email: " + email);
+        return true;
+    }
 }
+
--- src/pmFactory.java
+++ src/pmFactory.java
@@ -1,13 +1,21 @@
 public class pmFactory {
 
-    public PaymentMethod createPM(String choice){
+    public PaymentMethod createPM(String choice) throws IllegalArgumentException {
         PaymentMethod pm;
-        if (choice.equals("z")){
-            pm = new zenMoMethod();
-        }
-        else{
-            pm = new creditCardMethod();
+        switch (choice) {
+            case "c":
+                pm = new creditCardMethod();
+                break;
+            case "z":
+                pm = new zenMoMethod();
+                break;
+            case "p":
+                pm = new PayBuddyMethod("example@paybuddy.com");
+                break;
+            default:
+                throw new IllegalArgumentException("Invalid payment method code");
         }
         return pm;
     }
 }
+
